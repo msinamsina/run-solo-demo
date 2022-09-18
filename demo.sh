68433a155e71ed24d99a206f7506d9c4881cbf59
Mohammad sina Allahkaram <msinamsina@gmail.com>
2022-09-18 13:59:59 -0400

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 python -m pip install detectron2 -f  https://dl.fbaipublicfiles.com/detectron2/wheels/cu111/torch1.8/index.html
 python -m pip install torch==1.8.2 torchvision==0.9.2 torchaudio==0.8.2 --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
-git clone https://github.com/msinamsina/AdelaiDet.git
-cd AdelaiDet
+git clone https://github.com/msinamsina/AdelaiDet-M.git
+cd AdelaiDet-M
 python setup.py build develop
 
 # If you want, change weights with your training weights
